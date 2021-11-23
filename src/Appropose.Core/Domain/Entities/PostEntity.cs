78f276501aaa43d642ab06301138c5bbd5c70bec
snajsztub <snajsztub@objectivity.co.uk>
2021-11-23 09:47:01 +0000

    Post entity

--- src/Appropose.Core/Domain/Entities/PostEntity.cs
+++ src/Appropose.Core/Domain/Entities/PostEntity.cs
@@ -1,12 +1,28 @@
-﻿using Newtonsoft.Json;
-using ToDoList.Core.Domain.Entities;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace Appropose.Core.Domain.Entities
 {
     public class PostEntity : BaseEntity
     {
-        
-        [JsonProperty("description")]
+        [JsonProperty("title")]
         public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("imagePath")]
+        public string ImagePath { get; set; }
+
+        [JsonProperty("localization")]
+        public string Localization { get; set; }
+
+        [JsonProperty("angryCount")]
+        public int AngryCount { get; set; }
+
+        [JsonProperty("solutions")]
+        public Guid[] Solutions { get; set; }
+
     }
+
 }
