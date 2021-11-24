7c0b1bd6c30fff6ce2bdd52c5d9b3e190ec3d76a
snajsztub <snajsztub@objectivity.co.uk>
2021-11-24 16:04:26 +0000

    Finished fetching of solutions

--- src/Appropose.Functions/AzureFunctions/Get/GetSolutionsForPostFunction.cs
+++ src/Appropose.Functions/AzureFunctions/Get/GetSolutionsForPostFunction.cs
@@ -1,34 +1,30 @@
-using System.IO;
 using System.Threading.Tasks;
+using Appropose.Functions.Extensions;
+using Appropose.Functions.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Appropose.Functions.AzureFunctions
 {
-    public static class GetSolutionsForPostFunction
+    public class GetSolutionsForPostFunction
     {
-        [FunctionName("GetSolutionsForPostFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        private readonly IMediator _mediator;
+
+        public GetSolutionsForPostFunction(IMediator mediator)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            _mediator = mediator;
+        }
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+        [FunctionName("GetSolutionsForPostFunction")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
+        {
+            var query = new GetSolutionsForPostQuery(req.Query["postId"]);
+            var result = await _mediator.Send(query);
+            return result.IsFailed ? result.GetErrorResponse() : new OkObjectResult(result.Value);
         }
     }
 }
--- src/Appropose.Functions/Queries/GetAllPostsQuery.cs
+++ src/Appropose.Functions/Queries/GetAllPostsQuery.cs
@@ -21,6 +21,7 @@ namespace Appropose.Functions.Queries
         public string Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Question { get; set; }
         public string ImageUrl { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
